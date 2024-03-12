a52706791325a8bf0c5182db5f745701b40304a9
Jakub Madejczyk <jbmk01@gmail.com>
2024-03-13 00:39:24 +0100

    modal photos width finally fixed

--- app/ui/modal.tsx
+++ app/ui/modal.tsx
@@ -2,7 +2,7 @@
 import { useSearchParams, usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
-
+import { useState } from "react";
 interface FetchPhotosConfig {
   photos: {
     name: string;
@@ -21,24 +21,32 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
   const pathname = usePathname();
 
   const photoObj = props.photos_json.photos.find((el) => el.name === name);
-
+  // ?????
+  // const [photoObj, setPhotoObject] = useState(
+  //   props.photos_json.photos.find((el) => el.name === name)
+  // );
+  // ?????
   return (
     <>
       {modal && (
-        <dialog className="fixed top-0 left-0 w-full h-full bg-dark-gray bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
-          <div className="bg-[#ffffff] m-auto p-8">
+        <dialog className="fixed top-0 left-0 w-screen h-screen bg-dark-gray bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center">
+          <div className="bg-transparent m-auto p-8">
             <div className="flex flex-col items-center">
               <h3>Modal content</h3>
-              <Image
-                src={name ? `${folder}/${name}` : `/portraits/Img0801_.jpg`}
-                height={photoObj?.height}
-                width={photoObj?.width}
-                alt={photoObj?.desc ? photoObj?.desc : ""}
-                placeholder="blur"
-                blurDataURL={photoObj?.blured}
-                className={`m-auto max-h-full w-auto h-[1200px] max-w-none`}
-                quality={90}
-              />
+              <div>
+                <Image
+                  src={name ? `${folder}/${name}` : `/portraits/Img0801_.jpg`}
+                  height={photoObj?.height}
+                  width={photoObj?.width}
+                  alt={photoObj?.desc ? photoObj?.desc : ""}
+                  placeholder="blur"
+                  blurDataURL={photoObj?.blured}
+                  // className={""}
+                  className="max-h-[80vh] w-auto"
+                  quality={90}
+                />
+              </div>
+
               <br />
               <p>{`${
                 props.photos_json.photos.find((el) => el.name === name)?.name
