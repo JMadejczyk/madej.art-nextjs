ca36c2e6e006fa164b9f84d46a3d60dcd95c0905
Jakub Madejczyk <jbmk01@gmail.com>
2024-03-27 23:21:52 +0100

    modal animation added, right and left photo

--- app/ui/modal.tsx
+++ app/ui/modal.tsx
@@ -3,9 +3,10 @@
 import { useSearchParams, usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FiXSquare, FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { useRouter } from "next/navigation";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface FetchPhotosConfig {
   photos: {
@@ -30,26 +31,32 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
   const nextPhotoName = props.photos_json.photos[currPhotoId + 1]?.name;
   const prevPhotoName = props.photos_json.photos[currPhotoId - 1]?.name;
 
+  // let isNext: boolean = true;
+  const [isNext, setIsNext] = useState(true);
   // console.log(currPhotoId);
   const router = useRouter();
 
   console.log("Modal has been rerendered!");
 
-  const handleNextPhoto = () =>
+  const handleNextPhoto = () => {
+    setIsNext(true);
     router.push(
       nextPhotoName && folder
         ? pathname + `?modal=true&folder=${folder}&name=${nextPhotoName}`
         : pathname,
       { scroll: false }
     );
+  };
 
-  const handlePrevPhoto = () =>
+  const handlePrevPhoto = () => {
+    setIsNext(false);
     router.push(
       prevPhotoName && folder
         ? pathname + `?modal=true&folder=${folder}&name=${prevPhotoName}`
         : pathname,
       { scroll: false }
     );
+  };
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -78,57 +85,113 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
 
   return (
     <>
-      {modal && photoObj && (
-        <dialog
-          className="fixed top-0 left-0 w-screen h-screen bg-dark-gray bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center select-none	"
-          onClick={(e) => router.push(pathname, { scroll: false })}
-        >
-          <div className="bg-transparent m-auto p-8">
-            <div className="w-full flex justify-end">
+      <AnimatePresence>
+        {modal && photoObj && (
+          <motion.dialog
+            className="fixed top-0 left-0 w-screen h-screen bg-dark-gray bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center select-none	"
+            exit={{ backdropFilter: "none" }}
+            initial={{ backdropFilter: "none" }}
+            animate={{ backdropFilter: "blur(8px)" }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            onClick={(e) => router.push(pathname, { scroll: false })}
+            layout
+          >
+            <div className="bg-transparent m-auto p-8">
+              {/* <div className="w-full flex justify-end">
               <Link href={pathname} scroll={false} className="mb-[-4px]">
                 <button type="button">
                   <FiXSquare className="w-10 h-10 text-[#707070]" />
                 </button>
               </Link>
-            </div>
+            </div> */}
+              <motion.div
+                exit={{
+                  opacity: 0,
+                  // translateY: 1000,
+                  // translateX: 1000,
+                  scale: 0,
+                  // rotate: "2137deg",
+                }}
+                initial={{
+                  opacity: 0,
+                  // translateY: 1000,
+                  // translateX: 1000,
 
-            <div
-              className="relative h-full w-full"
-              onClick={(e) => e.stopPropagation()}
-            >
-              <div
-                className="absolute left-0 h-full w-[50%] text-transparent hover:text-[#707070]"
-                onClick={() => handlePrevPhoto()}
-              >
-                <FiChevronLeft className="w-10 h-10 absolute left-0 top-[45%]" />
-              </div>
-              <Image
-                src={`${folder}/${photoObj.name}`}
-                height={photoObj.height}
-                width={photoObj.width}
-                alt={photoObj.desc}
-                placeholder="blur"
-                blurDataURL={photoObj.blured}
-                priority={true}
-                // className={""}
-                className="max-h-[80vh] w-auto"
-                quality={90}
-              />
-              <div
-                className="absolute right-0 top-0 h-full w-[50%] text-transparent hover:text-[#707070]"
-                onClick={() => handleNextPhoto()}
-              >
-                <FiChevronRight className="w-10 h-10 absolute right-0 top-[45%]" />
-              </div>
-            </div>
+                  scale: 0,
+                  // rotate: "2137deg",
+                }}
+                animate={{
+                  opacity: 1,
+                  // translateY: 0,
+                  // translateX: 0,
 
-            <br />
-            {/* <p className="text-[#ffffff]">{`${
+                  scale: 1,
+                  // rotate: "0deg",
+                }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="relative h-full w-full"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div
+                  className="absolute left-0 h-full w-[50%] text-transparent hover:text-[#707070]"
+                  onClick={() => {
+                    setIsNext(false);
+                    handlePrevPhoto();
+                  }}
+                >
+                  <FiChevronLeft className="w-10 h-10 absolute left-0 top-[45%]" />
+                </div>
+                <AnimatePresence mode="wait">
+                  <motion.div
+                    initial={{
+                      translateX: isNext
+                        ? photoObj.width / 4
+                        : -photoObj.width / 4,
+                    }}
+                    animate={{ translateX: 0 }}
+                    exit={{
+                      translateX: isNext
+                        ? -photoObj.width / 4
+                        : photoObj.width / 4,
+                    }}
+                    transition={{ duration: 0.125 }}
+                    key={photoObj.name}
+                    layout
+                  >
+                    <Image
+                      src={`${folder}/${photoObj.name}`}
+                      height={photoObj.height}
+                      width={photoObj.width}
+                      alt={photoObj.desc}
+                      placeholder="blur"
+                      blurDataURL={photoObj.blured}
+                      priority={true}
+                      // className={""}
+                      className="max-h-[80vh] w-auto"
+                      quality={90}
+                      key={"image" + photoObj.name}
+                    />
+                  </motion.div>
+                </AnimatePresence>
+                <div
+                  className="absolute right-0 top-0 h-full w-[50%] text-transparent hover:text-[#707070]"
+                  onClick={() => {
+                    setIsNext(true);
+                    handleNextPhoto();
+                  }}
+                >
+                  <FiChevronRight className="w-10 h-10 absolute right-0 top-[45%]" />
+                </div>
+              </motion.div>
+
+              <br />
+              {/* <p className="text-[#ffffff]">{`${
               props.photos_json.photos.find((el) => el.name === name)?.name
             }`}</p> */}
-          </div>
-        </dialog>
-      )}
+            </div>
+          </motion.dialog>
+        )}
+      </AnimatePresence>
     </>
   );
 }
