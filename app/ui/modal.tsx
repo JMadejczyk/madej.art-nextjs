c08070d2e778bef57495f05eecb4f11d4b8d27c9
Jakub Madejczyk <jbmk01@gmail.com>
2024-03-28 02:16:27 +0100

    keydown cooldown added, code refactored

--- app/ui/modal.tsx
+++ app/ui/modal.tsx
@@ -3,7 +3,7 @@
 import { useSearchParams, usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { FiXSquare, FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { useRouter } from "next/navigation";
 import { motion, AnimatePresence } from "framer-motion";
@@ -31,45 +31,69 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
   const nextPhotoName = props.photos_json.photos[currPhotoId + 1]?.name;
   const prevPhotoName = props.photos_json.photos[currPhotoId - 1]?.name;
 
+  //Check if useState below is necessary
   const [isNext, setIsNext] = useState(true);
+
   // console.log(currPhotoId);
   const router = useRouter();
+  const cooldown = useRef(false);
 
-  console.log("Modal has been rerendered!");
+  // console.log("Modal has been rerendered!");
 
-  const handleNextPhoto = () => {
-    setIsNext(true);
-    router.push(
-      nextPhotoName && folder
-        ? pathname + `?modal=true&folder=${folder}&name=${nextPhotoName}`
-        : pathname,
-      { scroll: false }
-    );
+  const handleNextPhoto = (cooldownDuration: number = 0) => {
+    const doRouting = () => {
+      router.push(
+        nextPhotoName && folder
+          ? pathname + `?modal=true&folder=${folder}&name=${nextPhotoName}`
+          : pathname,
+        { scroll: false }
+      );
+    };
+    if (cooldownDuration === 0) {
+      setIsNext(true);
+      doRouting();
+    } else if (!cooldown.current) {
+      cooldown.current = true;
+      setIsNext(true);
+      doRouting();
+      setTimeout(() => {
+        cooldown.current = false;
+      }, cooldownDuration);
+    }
   };
 
-  const handlePrevPhoto = () => {
-    setIsNext(false);
-    router.push(
-      prevPhotoName && folder
-        ? pathname + `?modal=true&folder=${folder}&name=${prevPhotoName}`
-        : pathname,
-      { scroll: false }
-    );
+  const handlePrevPhoto = (cooldownDuration: number = 0) => {
+    const doRouting = () => {
+      router.push(
+        prevPhotoName && folder
+          ? pathname + `?modal=true&folder=${folder}&name=${prevPhotoName}`
+          : pathname,
+        { scroll: false }
+      );
+    };
+    if (cooldownDuration === 0) {
+      setIsNext(false);
+      doRouting();
+    } else if (!cooldown.current) {
+      cooldown.current = true;
+      setIsNext(false);
+      doRouting();
+      setTimeout(() => {
+        cooldown.current = false;
+      }, cooldownDuration);
+    }
   };
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === "ArrowRight") {
-        // console.log("handleNextPhoto");
-        handleNextPhoto();
+        handleNextPhoto(500);
       } else if (e.key === "ArrowLeft") {
-        // console.log("handlePrevPhoto");
-        handlePrevPhoto();
+        handlePrevPhoto(500);
       }
     };
     const document = window.document;
     document.addEventListener("keydown", handleKeyDown);
-
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, [
     modal,
@@ -87,7 +111,7 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
       <AnimatePresence>
         {modal && photoObj && (
           <motion.div
-            className="fixed top-0 left-0 h-screen w-full flex justify-center items-center"
+            className="fixed top-0 left-0 h-screen w-full flex justify-center items-center focus:outline-none outline-dark-gray"
             onClick={(e) => router.push(pathname, { scroll: false })}
             exit={{ backdropFilter: "none", backgroundColor: "rgb(0 0 0 0)" }}
             initial={{
@@ -98,59 +122,27 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
               backdropFilter: "blur(8px)",
               backgroundColor: "rgb(32 32 32 0.5)",
             }}
-            transition={{ duration: 0.3, ease: "easeInOut" }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
           >
-            <motion.dialog
-              className=" bg-transparent bg-opacity-0 z-50  
-              flex justify-center items-center select-none"
-              // backdrop-blur
-
-              layout
-            >
-              <div className="bg-transparent m-auto p-8">
-                {/* <div className="w-full flex justify-end">
-              <Link href={pathname} scroll={false} className="mb-[-4px]">
-                <button type="button">
-                  <FiXSquare className="w-10 h-10 text-[#707070]" />
-                </button>
-              </Link>
-            </div> */}
+            <dialog className="bg-transparent bg-opacity-0 z-50 flex justify-center items-center select-none">
+              <div className="bg-transparent m-auto p-8 ">
                 <motion.div
                   exit={{
                     opacity: 0,
-                    // translateY: 1000,
-                    // translateX: 1000,
-                    scale: 0,
-                    // rotate: "2137deg",
+                    scale: 0.3,
                   }}
                   initial={{
                     opacity: 0,
-                    // translateY: 1000,
-                    // translateX: 1000,
-
-                    scale: 0,
-                    // rotate: "2137deg",
+                    scale: 0.3,
                   }}
                   animate={{
                     opacity: 1,
-                    // translateY: 0,
-                    // translateX: 0,
-
                     scale: 1,
-                    // rotate: "0deg",
                   }}
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  transition={{ duration: 0.4, ease: "easeInOut" }}
                   className="relative h-full w-full"
                   onClick={(e) => e.stopPropagation()}
                 >
-                  <div
-                    className="absolute left-0 h-full w-[50%] text-transparent hover:text-[#707070]"
-                    onClick={() => {
-                      handlePrevPhoto();
-                    }}
-                  >
-                    <FiChevronLeft className="w-10 h-10 absolute left-0 top-[45%]" />
-                  </div>
                   <AnimatePresence mode="wait">
                     <motion.div
                       initial={{
@@ -166,8 +158,14 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
                       }}
                       transition={{ duration: 0.125 }}
                       key={photoObj.name}
-                      layout
                     >
+                      <div
+                        className="absolute left-0 h-full w-[50%] text-transparent hover:text-[#707070]"
+                        onClick={() => handlePrevPhoto()}
+                      >
+                        <FiChevronLeft className="w-10 h-10 absolute left-0 top-[45%]" />
+                      </div>
+
                       <Image
                         src={`${folder}/${photoObj.name}`}
                         height={photoObj.height}
@@ -176,29 +174,22 @@ function Modal(props: { photos_json: FetchPhotosConfig }) {
                         placeholder="blur"
                         blurDataURL={photoObj.blured}
                         priority={true}
-                        // className={""}
                         className="max-h-[80vh] w-auto"
                         quality={90}
                         key={"image" + photoObj.name}
                       />
+
+                      <div
+                        className="absolute right-0 top-0 h-full w-[50%] text-transparent hover:text-[#707070]"
+                        onClick={() => handleNextPhoto()}
+                      >
+                        <FiChevronRight className="w-10 h-10 absolute right-0 top-[45%]" />
+                      </div>
                     </motion.div>
                   </AnimatePresence>
-                  <div
-                    className="absolute right-0 top-0 h-full w-[50%] text-transparent hover:text-[#707070]"
-                    onClick={() => {
-                      handleNextPhoto();
-                    }}
-                  >
-                    <FiChevronRight className="w-10 h-10 absolute right-0 top-[45%]" />
-                  </div>
                 </motion.div>
-
-                <br />
-                {/* <p className="text-[#ffffff]">{`${
-              props.photos_json.photos.find((el) => el.name === name)?.name
-            }`}</p> */}
               </div>
-            </motion.dialog>
+            </dialog>
           </motion.div>
         )}
       </AnimatePresence>
