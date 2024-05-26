69b4c84b850f73877b7aa9cababa505ffeccca08
Jakub Madejczyk <jbmk01@gmail.com>
2024-05-26 20:58:45 +0200

    routes added

---
+++ backend/routes/auth.js
@@ -0,0 +1,46 @@
+const { Router } = require("express");
+const bcrypt = require("bcrypt");
+const sqlite3 = require("sqlite3").verbose();
+
+const router = Router();
+
+router.post("/login", async (req, res) => {
+  let db = new sqlite3.Database(
+    "./backend/database/portfolio.db",
+    sqlite3.OPEN_READONLY,
+    (err) => {
+      if (err) {
+        console.error(err.message);
+      }
+    }
+  );
+  const { username, password } = req.body;
+  // console.log(username, password, req.body);
+
+  const sql = `SELECT username, password FROM users WHERE username = ?`;
+
+  db.get(sql, username, (err, row) => {
+    if (err) {
+      throw err;
+    }
+
+    if (row) {
+      bcrypt.compare(password, row.password, function (err, result) {
+        if (result) {
+          // Passwords match
+          res.status(200).send({ message: "User authenticated" });
+        } else {
+          // Passwords don't match
+          bcrypt.hash(password, 10, function (err, hash) {
+            // console.log(hash, row.password);
+          });
+          res.status(401).send({ message: "Invalid password" });
+        }
+      });
+    } else {
+      res.status(401).send({ message: "User not found" });
+    }
+  });
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded());
+app.use(cookieParser());
+
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(
+  session({
+    secret: "Super secret secret",
+    // cookie: {},
+    resave: false,
+    saveUninitialized: false,
+    // store: new SequelizeStore({
+    //   db: sequelize,
+    // }),
+  })
+);
+
+////////////////////////////////////////////////////////////////////
+
+// app.get("/", (req, res) => {
+//   let db = new sqlite3.Database(
+//     "./portfolio.db",
+//     sqlite3.OPEN_READONLY,
+//     (err) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//     }
+//   );
+
+//   let sql = `SELECT * FROM photos`;
+
+//   db.all(sql, [], (err, rows) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.send(rows);
+//   });
+
+//   db.close((err) => {
+//     if (err) {
+//       console.error(err.message);
+//     }
+//   });
+// });
+
+////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////
+
+app.use("/api/auth", authRoute);
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
